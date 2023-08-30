bc23bc8e036eab1fc66229ee7dc68bc5351ca909
Meriem Soubih <soubih.meriem@gmail.com>
2023-08-30 19:59:25 +0100

    project update

---
+++ service/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+const PORT = 4040;
+
+let posts = fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf-8');
+posts = JSON.parse(posts);
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+
