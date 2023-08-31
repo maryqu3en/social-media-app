03e44c30b35615c9a63bf68656df03d23b18e1da
Meriem Soubih <soubih.meriem@gmail.com>
2023-08-31 11:12:42 +0100

    app.js update routers

--- service/index.js
+++ service/index.js
@@ -5,12 +5,24 @@ const cors = require("cors");
 const app = express();
 const PORT = 4040;
 
-let posts = fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf-8');
-posts = JSON.parse(posts);
-
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
+
+let posts = fs.readFileSync(path.resolve(__dirname, 'data.json'), 'utf-8');
+posts = JSON.parse(posts);
+
+app.get('/api', (req, res) => {
+  return res.status(200).json({ data: 'hello world' })
+});
+
+app.get('/api/posts', (req, res) => {
+  return res.status(200).json({
+    posts,
+  });
+});
+
+
 
 
 
