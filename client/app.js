9a5d608373d570c4086b3554cdd3aa76f271ab48
Meriem Soubih <soubih.meriem@gmail.com>
2023-09-01 19:55:29 +0100

    updated app.js

--- client/app.js
+++ client/app.js
@@ -12,7 +12,7 @@ let commentPopupStatus = false;
 let posts = [];
 
 //display create-post popup
-createPost.addEventListener('click', () => {
+createPostBtn.addEventListener('click', () => {
     if (postPopupStatus) {
         postPopup.style.display = 'none';
     } else {
@@ -21,16 +21,35 @@ createPost.addEventListener('click', () => {
     postPopupStatus = !postPopupStatus;
 });
 
-cancel.addEventListener('click', () => {
-    popup.style.display = 'none';
-    if(postPopupStatus){
-        postPopupStatus = !postPopupStatus;
-        return;
-    }
-    if (commentPopupStatus) {
-        commentPopupStatus = !commentPopupStatus;
-    }
+//hide popup after clicking cancel
+cancelPost.addEventListener('click', () => {
+    postPopup.style.display = 'none';
+    postPopupStatus = !postPopupStatus;
+    // commentPopupStatus = !commentPopupStatus;
+    
 });
+
+//create post element
+const createTicketDom = (id, title, isTodo) => {
+  const ticket = document.createElement('div');
+  ticket.classList.add('ticket');
+  ticket.id = id;
+  ticket.innerHTML = `<p>${title}</p>`;
+  if (isTodo) {
+      ticket.addEventListener('click', async (e) => {
+          await fetch(`${url}api/done`, {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ id: id })
+          });
+
+          location.reload();
+      });
+  }
+  return ticket;
+}
 
 
 
